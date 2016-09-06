cda802cff519b18a058e533c5fd04a44c668a64f
Tim Smith <tsmith@chef.io>
2016-09-06 15:29:45 -0700

    Remove Chef 11 support and the need for the Windows cookbook Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,8 @@ description       'Installs Microsoft .NET 4.5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.0.2'
 supports          'windows'
-depends           'windows'
 
-source_url 'https://github.com/chef-cookbooks/ms_dotnet45' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/ms_dotnet45/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/ms_dotnet45'
+issues_url 'https://github.com/chef-cookbooks/ms_dotnet45/issues'
+
+chef_version '>= 12'
